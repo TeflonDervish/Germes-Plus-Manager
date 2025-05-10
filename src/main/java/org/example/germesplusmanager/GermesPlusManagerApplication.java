2bd017b5e60550952ffc61b4abc85ec3fae8d997
teflondervish <slava.semenov03@mail.ru>
2025-05-10 22:39:31 +0500

    Initial commit

---
+++ src/main/java/org/example/germesplusmanager/GermesPlusManagerApplication.java
@@ -0,0 +1,13 @@
+package org.example.germesplusmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GermesPlusManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GermesPlusManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/germesplusmanager/GermesPlusManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.germesplusmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GermesPlusManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
